a231c41c0d6fd4a254fbb2b2dd0bd9e76fd99da5
mauricio mejia <mauricio.mejia@koombea.com>
2013-04-26 00:03:20 -0500

    drag try

--- public/js/main.js
+++ public/js/main.js
@@ -14,4 +14,21 @@ $(function(){
 	   return false;
 	  
 	  });
+
+	  var startX, offsetX, el = $("li .ui-btn-inner");
+		el[0].addEventListener('touchstart', function(e) {
+		  e.preventDefault();
+		  offsetX = ($(window).width()-el.outerWidth(true))/2;
+		  startX = e.targetTouches[0].pageX - offsetX;
+		}, false);
+		el[0].addEventListener("touchmove", function(e) {
+		  e.preventDefault();
+		  var diffX = (e.changedTouches[0].pageX - offsetX) - startX;
+		  el.css("-webkit-transform", "translate3d("+diffX+"px, 0, 0)");
+		}, false);
+		el[0].addEventListener("touchend", function(e) {
+		  e.preventDefault();
+		  el.css("-webkit-transform", "translate3d(0, 0, 0)");
+		}, false);
+
 });
