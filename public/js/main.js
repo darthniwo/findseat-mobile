84358e0c66aaf6aa5ba92ff66244524ff81d260a
mauricio mejia <mauricio.mejia@koombea.com>
2013-04-26 00:58:35 -0500

    last night code

--- public/js/main.js
+++ public/js/main.js
@@ -15,20 +15,26 @@ $(function(){
 	  
 	  });
 
+
 	  var startX, offsetX, el = $("li .ui-btn-inner");
 		el[0].addEventListener('touchstart', function(e) {
 		  e.preventDefault();
 		  offsetX = ($(window).width()-el.outerWidth(true))/2;
 		  startX = e.targetTouches[0].pageX - offsetX;
+		  console.log('touchstart');
 		}, false);
 		el[0].addEventListener("touchmove", function(e) {
 		  e.preventDefault();
 		  var diffX = (e.changedTouches[0].pageX - offsetX) - startX;
 		  el.css("-webkit-transform", "translate3d("+diffX+"px, 0, 0)");
+		  console.log('touchmove');
 		}, false);
 		el[0].addEventListener("touchend", function(e) {
 		  e.preventDefault();
 		  el.css("-webkit-transform", "translate3d(0, 0, 0)");
+		  console.log('touchend');
 		}, false);
+
+
 
 });
