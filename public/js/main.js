b09485bba2ede8ddff10005bf91cc0307c4066c9
Mauricio <mauricio.mejia@koombea.com>
2013-04-26 12:50:30 -0500

    added slide behavior

--- public/js/main.js
+++ public/js/main.js
@@ -16,24 +16,29 @@ $(function(){
 	  });
 
 
-	  var startX, offsetX, el = $("li .ui-btn-inner");
-		el[0].addEventListener('touchstart', function(e) {
-		  e.preventDefault();
-		  offsetX = ($(window).width()-el.outerWidth(true))/2;
-		  startX = e.targetTouches[0].pageX - offsetX;
-		  console.log('touchstart');
-		}, false);
-		el[0].addEventListener("touchmove", function(e) {
-		  e.preventDefault();
-		  var diffX = (e.changedTouches[0].pageX - offsetX) - startX;
-		  el.css("-webkit-transform", "translate3d("+diffX+"px, 0, 0)");
-		  console.log('touchmove');
-		}, false);
-		el[0].addEventListener("touchend", function(e) {
-		  e.preventDefault();
-		  el.css("-webkit-transform", "translate3d(0, 0, 0)");
-		  console.log('touchend');
-		}, false);
+	  var startX, offsetX, els = document.querySelectorAll("li div.ui-btn-inner");
+	  for(var i = 0, len = els.length; i < len; i++ ){
+	  	console.log(els[i])
+	  	var el = els[i];
+			el.addEventListener('touchstart', function(e) {
+				console.log(this)
+			  e.preventDefault();
+			  offsetX = ($(window).width()-$(this).outerWidth(true))/2;
+			  startX = e.targetTouches[0].pageX - offsetX;
+			  console.log('touchstart');
+			}, false);
+			el.addEventListener("touchmove", function(e) {
+			  e.preventDefault();
+			  var diffX = (e.changedTouches[0].pageX - offsetX) - startX;
+			  $(this).css("-webkit-transform", "translate3d("+diffX+"px, 0, 0)");
+			  console.log('touchmove');
+			}, false);
+			el.addEventListener("touchend", function(e) {
+			  e.preventDefault();
+			  $(this).css("-webkit-transform", "translate3d(0, 0, 0)");
+			  console.log('touchend');
+			}, false);
+	  }
 
 
 
